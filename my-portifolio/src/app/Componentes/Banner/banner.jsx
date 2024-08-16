21656b6ae36c8dd61d222e97811da144dcdef302
Your Name <you@example.com>
2024-08-16 18:34:56 -0300

    criando a parte de links e icons

--- my-portifolio/src/app/Componentes/Banner/banner.jsx
+++ my-portifolio/src/app/Componentes/Banner/banner.jsx
@@ -7,10 +7,23 @@ export default function Banner(props){
     return(
         <section className={style.containerGlobalBanner}>
             <div className={style.containerImage}>
-                <Imagem src={props.ImagemBanner} alt={props.titulo} title={props.titulo}/>
+                <Image className={style.image} src={props.imagemBanner} alt={props.titulo} title={props.titulo}/>
             </div>
             <div className={style.containerInformacoesBanner}>
-                
+                <div>
+                    <span className={style.span}>Hellow, I'm</span>
+                    <h2 className={style.h2}>Tarcisio H</h2>
+                    <p className={style.p}> Frontend Developer </p>
+                </div>
+                <div className={style.containerLinksBanner}>
+                   <div className={style.containerBtnBanner}>
+                       <a href="../../curriculo/TarcisioHenriqueCurriculo.pdf">Curriculo</a>
+                       <a href="#">Contact info</a>
+                   </div>
+                   <div className={style.containerIconBanner}>
+
+                   </div>
+                </div>
             </div>
 
         </section>
--- my-portifolio/src/app/page.js
+++ my-portifolio/src/app/page.js
@@ -2,11 +2,15 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 import Header from "./Componentes/Header/header";
+import Banner from "./Componentes/Banner/banner";
+
+import FotoPerfil from "../../public/fotoPerfil.jpg";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header/>
+      <Banner imagemBanner={FotoPerfil} titulo="Tarcisio H"/>
     </main>
   );
 }
