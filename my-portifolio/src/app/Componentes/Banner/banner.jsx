9ae38763d52eea7ec3be35e28516f50c88e59373
Your Name <you@example.com>
2024-08-19 09:09:54 -0300

    subindo alterações

--- my-portifolio/src/app/Componentes/Banner/banner.jsx
+++ my-portifolio/src/app/Componentes/Banner/banner.jsx
@@ -17,24 +17,25 @@ export default function Banner(props){
                 <Image className={style.image} src={props.imagemBanner} alt={props.titulo} title={props.titulo}/>
             </div>
             <div className={style.containerInformacoesBanner}>
-                <div>
+                <article>
                     <span className={style.span}>Hellow, I'm</span>
                     <h2 className={style.h2}>Tarcisio H</h2>
                     <p className={style.p}> Frontend Developer </p>
-                </div>
+                </article>
                 <div className={style.containerLinksBanner}>
-                   <div className={style.containerCurriculoBanner}>
+                   <article className={style.containerCurriculoBanner}>
                        {BaixarCv()}
                        <Link className={style.linkContact} href="#">Contact info</Link>
-                   </div>
-                   <div className={style.containerIconBanner}>
+                   </article>
+                   <article className={style.containerIconBanner}>
                     <Link className={style.iconGithub} target="_blank" href="https://github.com/tarcisiohenriqueassis"><Image className={style.icon} src={IconGithub} width={40} height={40}/></Link>
-                    <Link className={style.iconLinkedin} href="www.linkedin.com/in/tarcisiohenriqueassis"><Image className={style.icon} src={IconLinkedin} width={40} height={40}/></Link>
+                    <Link className={style.iconLinkedin} href="https://www.linkedin.com/in/tarcisiohenriqueassis/"><Image className={style.icon} src={IconLinkedin} width={40} height={40}/></Link>
                     <Link className={style.iconWathSapp} href="tel:+5533987054654"><Image className={style.icon} src={IconWhatsapp} width={40} height={40}/></Link>
-                   </div>
+                   </article>
                 </div>
-            </div>
 
-        </section>
+            </div>
+            </section>
+    
     )
 }
--- my-portifolio/src/app/page.js
+++ my-portifolio/src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 import Header from "./Componentes/Header/header";
 import Banner from "./Componentes/Banner/banner";
 
-import FotoPerfil from "../../public/fotoPerfil.jpg";
+import FotoPerfil from "../../public/fotoPerfil.png";
 
 export default function Home() {
   return (
