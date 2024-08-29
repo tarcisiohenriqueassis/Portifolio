fe64b9b9da81c43ebf6b75ec2dd61b5c0c2d85b1
Your Name <you@example.com>
2024-08-29 13:15:06 -0300

    subindo melhorias

--- my-portifolio/src/app/Componentes/CardExperience/cardExperience.jsx
+++ my-portifolio/src/app/Componentes/CardExperience/cardExperience.jsx
@@ -1,11 +1,17 @@
+import Image from "next/image";
+
 import style from "../CardExperience/cardExperience.module.css";
 
-export default function CardExperience(){
+import IconCheck from "../../../../public/checkmark.png";
+
+export default function CardExperience(props){
     return(
-        <section className={style.containerGlobalCard}>
             <article className={style.card}>
-                
+                <Image className={style.imagem} src={props.iconLinguagem} width={50} height={50}/>
+                <div>
+                    <h3 className={style.titulo}>{props.linguagem}</h3>
+                    <p className={style.p}>{props.nivelExperiencia}</p>
+                </div>
             </article>
-        </section>
     )
 }
--- my-portifolio/src/app/page.js
+++ my-portifolio/src/app/page.js
@@ -8,24 +8,31 @@ import Header from "./Componentes/Header/header";
 import Banner from "./Componentes/Banner/banner";
 import CardSobreMim from "./Componentes/CardSobreMim/card";
 import CardExperience from "./Componentes/CardExperience/cardExperience";
-//imagens
+//imagem perfil Banner
 import FotoPerfil from "../../public/fotoPerfil.jpg";
+//imagens da seção about 
 import IconEducation from "../../public/education.png";
 import IconExperience from "../../public/experience.png";
 import IconScrollPage from "../../public/arrow.png";
+//imagens da seção experience
+import IconHtml from "../../public/iconHtml.png";
+import IconCss from "../../public/iconCss.webp";
+import IconReact from "../../public/iconReact.png";
+import IconNodejs from "../../public/iconNodejs.png";
+import IconJs from "../../public/iconJs.png";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header/>
       <Banner id="Banner" imagemBanner={FotoPerfil} titulo="Tarcisio H"/>
-      <section className={styles.containerSectionCardSobreMim}>
+      <section className={styles.containerSectionGlobalCardSobreMim}>
       
         <article className={styles.containerTitulo}>
           <span className={styles.spanSections}>Get To Know More</span>
           <h2 className={styles.tituloSections}>About Me</h2>
         </article>
-        <article className={styles.containerCards}>
+        <article className={styles.containerCardsSobreMim}>
           <CardSobreMim imagemCard={IconExperience} titulo="Experience" span="1+ year" paragrafo="Frontend Development"/>
           <CardSobreMim imagemCard={IconEducation} titulo="Education"span="Ensino Medio - Completo" paragrafo="E.E.Labor Club"/>  
         </article>
@@ -35,13 +42,30 @@ export default function Home() {
           </Link>
         </article>
       </section> 
-      <section id="Experience" className={styles.containerSectionExperiencia}>
+      <section id="Experience" className={styles.containerGlobalSectionExperience}>
         <article className={styles.containerTitulo}>
           <span className={styles.spanSections}>Explore My</span>
           <h2 className={styles.tituloSections}>Experience</h2>
         </article>
-        <article className={styles.containerCardsExperience}>
-
+        <article className={styles.containerCardsLinguagens}>
+          <div className={styles.containerGlobalCardsFrontEnd}>
+            <h3>Frontend</h3>
+              <article className={styles.containerCards}>
+                <CardExperience linguagem="Html" nivelExperiencia="avançado" iconLinguagem={IconHtml} />
+                <CardExperience linguagem="Css" nivelExperiencia="intermediario" iconLinguagem={IconCss}/>
+                <CardExperience linguagem="React.js" nivelExperiencia="intermediario" iconLinguagem={IconReact}/>
+                <CardExperience linguagem="JavaScript" nivelExperiencia="basico" iconLinguagem={IconJs}/>
+              </article>   
+          </div>
+          <div className={styles.containerGlobalCardsBackend}>
+            <h3>Backend</h3>
+              <article className={styles.containerCards}>
+                <CardExperience linguagem="Html" nivelExperiencia="avançado" iconLinguagem={IconHtml} />
+                <CardExperience linguagem="Css" nivelExperiencia="intermediario" iconLinguagem={IconCss}/>
+                <CardExperience linguagem="React.js" nivelExperiencia="intermediario" iconLinguagem={IconReact}/>
+                <CardExperience linguagem="JavaScript" nivelExperiencia="basico" iconLinguagem={IconJs}/>
+              </article>   
+          </div>
         </article>
       </section>
      
